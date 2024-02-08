22a232e699ffa430b50929d3e4133612220f6d78
anupamhaldkar <anupam.haldkar@outlook.com>
2024-02-08 13:26:33 +0530

    implemented: userList table data styling button

--- components/UserList.js
+++ components/UserList.js
@@ -30,10 +30,13 @@ const UserList = () => {
                             email@fmfsds.com
                             </div>
                         </td>
-                        <td className='text-left px-6 py-4 whitespace-nowrap'>
-                            <div className='text-sm text-gray-500'>
-                            button
-                            </div>
+                        <td className='text-right px-6 py-4 whitespace-nowrap'>
+                            <a href='#' className='text-indigo-600 hover:text-indigo-800 cursor-pointer px-4'>
+                                Edit
+                            </a>
+                            <a href='#' className='text-indigo-600 hover:text-indigo-800 cursor-pointer'>
+                                Delete
+                            </a>
                         </td>
                     </tr>
                 </tbody>
