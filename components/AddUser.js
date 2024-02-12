d70473ed0776c6eeb74487b5c16cd464e8e3db1d
anupamhaldkar <anupam.haldkar@outlook.com>
2024-02-12 19:53:18 +0530

    implemented: dialog for adding new user

--- components/AddUser.js
+++ components/AddUser.js
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 
 const AddUser = () => {
-  return (
-    <div className='container mx-auto my-8'>
-        <div className='h-12'>
-            <button className='rounded bg-slate-600 text-white px-6 py-2 font-semibold'>Add User</button>
-        </div>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default AddUser
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+  function openModal(){
+    setIsOpen(true);
+  }
+  
+  return (
+    <>
+      <div className="container mx-auto my-8">
+        <div className="h-12">
+          <button
+            onClick={openModal}
+            className="rounded bg-slate-600 text-white px-6 py-2 font-semibold"
+          >
+            Add User
+          </button>
+        </div>
+      </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all bg-white shadow-xl rounded-md">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Add new User
+                </Dialog.Title>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default AddUser;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Navbar from "../../components/Navbar";
 import UserList from "../../components/UserList";
 import AddUser from "../../components/AddUser";
-import { Transition } from "@headlessui/react";
-import { Fragment } from "react";
 
 
 export default function Home() {
@@ -19,7 +17,6 @@ export default function Home() {
       <UserList />
     </main>
     </div>
-    <Transition appear show={isOpen} as={Fragment}> </Transition>
     </>
   );
 }
