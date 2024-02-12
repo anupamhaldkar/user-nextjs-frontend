83d816b92e1c8da0a91b41fe4edec8b7fa9d33c4
anupamhaldkar <anupam.haldkar@outlook.com>
2024-02-12 14:32:22 +0530

    implemented: added transition component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import Head from "next/head";
 import Navbar from "../../components/Navbar";
 import UserList from "../../components/UserList";
 import AddUser from "../../components/AddUser";
+import { Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 
 export default function Home() {
   return (
+    <>
     <div>
       <Head>
         <title>User Management App</title>
@@ -16,5 +19,7 @@ export default function Home() {
       <UserList />
     </main>
     </div>
+    <Transition appear show={isOpen} as={Fragment}> </Transition>
+    </>
   );
 }
