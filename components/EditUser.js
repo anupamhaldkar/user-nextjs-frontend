89a89f7e0b5df1effe59cb3d56445b73a9e4cf6c
anupamhaldkar <anupam.haldkar@outlook.com>
2024-02-17 23:21:41 +0530

    implemented: edit component

---
+++ components/EditUser.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+
+const EditUser = ({ userId }) => {
+  const USER_BASE_API_URL = "http://localhost:8080/api/v1/users";
+  const [isOpen, setIsOpen] = useState(false);
+  const [user, setUser] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    emailId: "",
+  });
+
+  const [responseUser, setResponseUser] = useState({
+    id: "",
+    firstName: "",
+    lastName: "",
+    emailId: "",
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch(USER_BASE_API_URL + "/" + userId, 
+            {method: "GET",
+            headers: {
+                "Content-Type": "application/json
+            }
+        });
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    const _user = await response.json();
+    setUser(_user);
+    setIsOpen(true);
+  }, [])
+  
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const handleChange = ({ target }) => {
+    setUser({ ...user, [target.name]: target.value });
+  };
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog
+        as="div"
+        className="fixed inset-0 z-10 overflow-y-auto"
+        onClose={closeModal}
+      >
+        <div className="min-h-screen px-4 text-center">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all bg-white shadow-xl rounded-md">
+              <Dialog.Title
+                as="h3"
+                className="text-lg font-medium leading-6 text-gray-900"
+              >
+                Add new User
+              </Dialog.Title>
+              <div className="flex min-w-md mx-auto">
+                <div className="py-2">
+                  <div className="h-14 my-4">
+                    <label className="block text-gray-600 text-sm font-normal">
+                      First Name
+                    </label>
+                    <input
+                      type="text"
+                      name="firstName"
+                      value={user.firstName}
+                      onChange={(e) => handleChange(e)}
+                      className="h-10 w-96 border mt-2 px-2 py-2"
+                    ></input>
+                  </div>
+                  <div className="h-14 my-4">
+                    <label className="block text-gray-600 text-sm font-normal">
+                      Last Name
+                    </label>
+                    <input
+                      type="text"
+                      name="lastName"
+                      value={user.lastName}
+                      onChange={(e) => handleChange(e)}
+                      className="h-10 w-96 border mt-2 px-2 py-2"
+                    ></input>
+                  </div>
+                  <div className="h-14 my-4">
+                    <label className="block text-gray-600 text-sm font-normal">
+                      Email Id
+                    </label>
+                    <input
+                      type="text"
+                      name="emailId"
+                      value={user.emailId}
+                      onChange={(e) => handleChange(e)}
+                      className="h-10 w-96 border mt-2 px-2 py-2"
+                    ></input>
+                  </div>
+                  <div className="h-14 my-4 space-x-4 pt-4">
+                    <button
+                      onClick={saveUser}
+                      className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6"
+                    >
+                      Save
+                    </button>
+                    <button
+                      onClick={reset}
+                      className="rounded text-white font-semibold bg-red-400 hover:bg-red-700 py-2 px-6"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
+
+export default EditUser;
